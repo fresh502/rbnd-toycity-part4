3c2ac7e607a093aec0059a45b0fff0eb29f2a3f1
fresh502 <fresh502@gmail.com>
2016-04-13 22:28:54 +0900

    Add create method

--- lib/product.rb
+++ lib/product.rb
@@ -17,6 +17,11 @@ class Product < Udacidata
     @price = opts[:price]
   end
 
+  def self.create(opts={})
+    opts[:allow_duplicates] = false
+    super
+  end
+
   private
 
     # Reads the last line of the data file, and gets the id if one exists
--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -3,5 +3,22 @@ require_relative 'errors'
 require 'csv'
 
 class Udacidata
-  # Your code goes here!
+	def self.create(opts={})
+		data = self.new(opts)
+		@data_path = File.dirname(__FILE__) + "/../data/data.csv"
+
+		CSV.open(@data_path, "a+") do |csv|
+			duplication = false
+			unless opts[:allow_duplicates]
+				csv.each do |row|
+					if (data.instance_variables.map { |instance_variable| self.instance_variable_get(instance_variable) } - row).empty?
+						duplication = true
+						break
+					end
+				end
+			end
+			csv << data.instance_variables.map { |instance_variable| data.instance_variable_get(instance_variable).to_s } unless duplication
+		end
+		return data
+	end
 end

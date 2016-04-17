64109fbfd8b8ee0e48bc3eef15d5242846b95ba9
fresh502 <fresh502@gmail.com>
2016-04-17 16:43:16 +0900

    Add methods for statistics

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -1,3 +1,44 @@
 module Analyzable
-  # Your code goes here!
+	def average_price(products)
+		(products.map(&:price).map(&:to_f).reduce { |total_price, price| total_price + price } / products.length).round(2)
+	end
+
+	def count_by_brand(products)
+		result = {}
+		products.each do |product|
+			if result.keys.include? product.brand
+				result[product.brand] += 1
+			else
+				result[product.brand] = 1
+			end
+		end
+		return result
+	end
+
+	def count_by_name(products)
+		result = {}
+		products.each do |product|
+			if result.keys.include? product.name
+				result[product.name] += 1
+			else
+				result[product.name] = 1
+			end
+		end
+		return result
+	end
+
+	def print_report(products)
+		average_price_result = average_price(products)
+		puts "====Inventory by Brand===="
+		count_by_brand(products).each do |key, value|
+			puts "#{key} : #{value}"
+		end
+		puts "====Inventory by name===="
+		count_by_name(products).each do |key, value|
+			puts "#{key} : #{value}"
+		end
+		puts "====Average_price===="
+		puts "$#{average_price_result}"
+		return average_price_result.to_s
+	end
 end
